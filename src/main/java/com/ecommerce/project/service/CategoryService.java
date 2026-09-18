ad0ba236ae88fbfe1663a9c7a127c9e73e19cc85
PedroL <pepluisgon01@gmail.com>
2026-09-18 16:21:44 -0300

    Add H2 console dependency and format CategoryService imports

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -1,11 +1,12 @@
 package com.ecommerce.project.service;
 
-import com.ecommerce.project.model.Category;
-
 import java.util.List;
+
+import com.ecommerce.project.model.Category;
 
 public interface CategoryService {
     List<Category> getAllCategories();
+
     void createCategory(Category category);
 
     String deleteCategory(Long categoryId);
